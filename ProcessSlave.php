82e2698fa94513445c42e3ecedaea92441c76a11
Marc J. Schmidt <marc@marcjschmidt.de>
2016-04-04 18:16:57 +0200

    Fixed typo and added missing test file :)

--- ProcessSlave.php
+++ ProcessSlave.php
@@ -391,8 +391,8 @@ class ProcessSlave
 
         //We receive X-PHP-PM-Remote-IP from ProcessManager.
         //This header is only used to proxy the remoteAddress from master -> slave.
-        $_SERVER['REMOTE_ADDR'] = $_SERVER['X_PHP_PM_REMOTE_IP'];
-        unset($_SERVER['X_PHP_PM_REMOTE_IP']);
+        $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_PHP_PM_REMOTE_IP'];
+        unset($_SERVER['HTTP_X_PHP_PM_REMOTE_IP']);
 
         $_SERVER['SERVER_NAME'] = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
         $_SERVER['REQUEST_URI'] = $request->getPath();
---
+++ tests/ProcessManagerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace PHPPM\Tests;
+
+class ProcessManagerTest extends PhpPmTestCase
+{
+
+    public function provideReplaceHeader()
+    {
+        return [
+            [
+                "Content-Type: plain/text\r\nX-Real-Ip: 1337.1337.1337.1337\r\n\r\n",
+                "Content-Type: plain/text\r\nX-Real-IP: 127.0.0.1\r\n\r\n",
+                ['X-Real-IP' => '127.0.0.1']
+            ],
+            [
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nConnection: keep-alive\r\n\r\n",
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nConnection: keep-alive\r\nX-PHP-PM-Remote-IP: 127.0.0.1\r\n\r\n",
+                ['X-PHP-PM-Remote-IP' => '127.0.0.1']
+            ],
+            [
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nConnection: keep-alive\r\nX-php-pm-REMOTE-IP: 137.137.137.137\r\n\r\n",
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nConnection: keep-alive\r\nX-PHP-PM-Remote-IP: 127.0.0.1\r\n\r\n",
+                ['X-PHP-PM-Remote-IP' => '127.0.0.1']
+            ],
+            [
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nX-php-pm-REMOTE-IP: 137.137.137.137\r\nConnection: keep-alive\r\n\r\n",
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nX-PHP-PM-Remote-IP: 127.0.0.1\r\nConnection: keep-alive\r\n\r\n",
+                ['X-PHP-PM-Remote-IP' => '127.0.0.1']
+            ],
+            [
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nX-php-pm-REMOTE-IP: 137.137.137.137\r\nConnection: keep-alive\r\n\r\n",
+                "GET /images/spinners/octocat-spinner-128.gif HTTP/1.1\r\nHost: assets-cdn.github.com\r\nX-PHP-PM-Remote-IP: 192.168.1.1\r\nConnection: keep-alive\r\n\r\n",
+                ['X-PHP-PM-Remote-IP' => '192.168.1.1']
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider provideReplaceHeader
+     * 
+     * @param string $originHeader
+     * @param string $expectedNewHeader
+     * @param array $replaceHeaders
+     */
+    public function testReplaceHeader($originHeader, $expectedNewHeader, array $replaceHeaders)
+    {
+        $replaceHeader = $this->getProcessManagerMethod('replaceHeader');
+        $replacedHeader = $replaceHeader($originHeader, $replaceHeaders);
+        $this->assertEquals($expectedNewHeader, $replacedHeader);
+    }
+
+    public function provideIsHeaderEnd()
+    {
+        return [
+            ["Content-Type: plain/text\r\nX-Real-Ip: 1337.1337.1337.1337\r\n\r\n", true],
+            ["Content-Type: plain/text\r\nX-Real-Ip: 1337.1337.1337.1337\r", false],
+            ["Content-Type: plain/text\r\nX-Real-Ip: 1337.1337.1337.1337\r\n", false],
+            ["Content-Type: plain/text\r\nX-Real-Ip: 1337.133", false],
+            ["Content-Type: plain/text\r\n\r\n", true],
+            ["Content-Type: plain/text\r\nX-Real-Ip: 1337.1337.1337.1337\r\n\r\nThis is some content", true],
+        ];
+    }
+
+    /**
+     * @dataProvider provideIsHeaderEnd
+     * 
+     * @param string $header
+     * @param boolean $isEnd
+     */
+    public function testIsHeaderEnd($header, $isEnd)
+    {
+        $isHeaderEnd = $this->getProcessManagerMethod('isHeaderEnd');
+        $this->assertEquals($isHeaderEnd($header), $isEnd);
+    }
+}
