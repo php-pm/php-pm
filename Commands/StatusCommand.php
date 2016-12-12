7d30b6566990e49be79010c2e7ccf0468b7b6dc5
Maciej Mroziński <maciej.k.mrozinski@gmail.com>
2016-12-12 15:52:23 +0100

    Add more data to status command, add status response array processing (#195)

--- Commands/StatusCommand.php
+++ Commands/StatusCommand.php
@@ -5,6 +5,7 @@ namespace PHPPM\Commands;
 use PHPPM\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class StatusCommand extends Command
@@ -20,6 +21,7 @@ class StatusCommand extends Command
 
         $this
             ->setName('status')
+            ->addOption('socket-path', null, InputOption::VALUE_OPTIONAL, 'Path to a folder where socket files will be placed. Relative to working-directory or cwd()', '.ppm/run/')
             ->addArgument('working-directory', null, 'working directory', './')
             ->setDescription('Status of all processes')
         ;
@@ -33,11 +35,27 @@ class StatusCommand extends Command
 
         $handler = new Client();
         $handler->setSocketPath($config['socket-path']);
-        $handler->getStatus(function($status) use ($output) {
-            foreach ($status as $key => $value) {
-                $output->writeln(sprintf('%s: %s', $key, $value));
-            }
+        $handler->getStatus(function ($status) use ($output) {
+            $output->writeln($this->parseStatus($status));
         });
+    }
+
+    /**
+     * @param array|string $status
+     * @param int $indentLevel
+     * @return string
+     */
+    private function parseStatus($status, $indentLevel = 0)
+    {
+        if (is_array($status)) {
+            $p = PHP_EOL;
+            foreach ($status as $key => $value) {
+                $p .= sprintf('%s%s: %s', str_repeat("\t", $indentLevel), $key, $this > parseStatus($value, $indentLevel + 1));
+            }
+        } else {
+            $p = $status . PHP_EOL;
+        }
+        return $p;
     }
 
 }
