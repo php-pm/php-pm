7b49d6b302f7e7576610128cd82db8501280df69
Niklas Keller <me@kelunik.com>
2017-02-16 16:55:11 +0100

    Generate proper session IDs (#207)
    Generates proper session IDs and fails closed if that's not possible. Before this patch the security of session IDs is highly dependent on the installed extensions and in the worst case falls back to a pure uniqid + microtime combination.

--- Utils.php
+++ Utils.php
@@ -70,22 +70,7 @@ class Utils
      */
     public static function generateSessionId()
     {
-        $entropy = '';
-
-        $entropy .= uniqid(mt_rand(), true);
-        $entropy .= microtime(true);
-
-        if (function_exists('openssl_random_pseudo_bytes')) {
-            $entropy .= openssl_random_pseudo_bytes(32, $strong);
-        }
-        
-        if (function_exists('mcrypt_create_iv')) {
-            $entropy .= mcrypt_create_iv(32, MCRYPT_DEV_URANDOM);
-        }
-
-        $hash = hash('whirlpool', $entropy);
-
-        return $hash;
+        return \bin2hex(\random_bytes(32));
     }
 
     /**
