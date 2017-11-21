cdbfe80aa0699e471d1990a00919b26c65464d06
andig <cpuidle@gmx.de>
2017-11-22 00:39:56 +0100

    Remove AsyncInterface (#288)

--- Bridges/BridgeInterface.php
+++ Bridges/BridgeInterface.php
@@ -3,7 +3,6 @@
 namespace PHPPM\Bridges;
 
 use Interop\Http\Server\RequestHandlerInterface;
-use React\EventLoop\LoopInterface;
 
 interface BridgeInterface extends RequestHandlerInterface
 {
@@ -13,8 +12,6 @@ interface BridgeInterface extends RequestHandlerInterface
      * @param string|null $appBootstrap The environment your application will use to bootstrap (if any)
      * @param string $appenv
      * @param boolean $debug If debug is enabled
-     * @param LoopInterface $loop
-     * @see http://stackphp.com
      */
-    public function bootstrap($appBootstrap, $appenv, $debug, LoopInterface $loop);
+    public function bootstrap($appBootstrap, $appenv, $debug);
 }
