d15775ebeb4d6723e41293cd23abd06c8284eb92
Viktor Szépe <viktor@szepe.net>
2018-01-08 18:51:25 +0100

    Clean for for ClosureHelper (#326)

--- src/Utils.php
+++ src/Utils.php
@@ -3,22 +3,6 @@
 namespace PHPPM;
 
 /**
- * Helper class to avoid creating closures in static context
- * See https://bugs.php.net/bug.php?id=64761
- */
-class ClosureHelper
-{
-    /**
-     * Return a closure that assigns a property value
-     */
-    public function getPropertyAccessor($propertyName, $newValue) {
-        return function () use ($propertyName, $newValue) {
-            $this->$propertyName = $newValue;
-        };
-    }
-}
-
-/**
  * Nitty gritty helper methods to hijack objects. Useful to reset properties that would otherwise run amok
  * and result in memory leaks.
  */
