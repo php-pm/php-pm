ad77de434947bdd5eace6e5f4d1661188f129418
Viktor Szépe <viktor@szepe.net>
2020-08-12 12:10:19 +0200

    Anti-var_dump Laser Gun (#516)
    * Anti-var_dump Laser Gun

--- .php_cs.php
+++ .php_cs.php
@@ -8,7 +8,10 @@ return PhpCsFixer\Config::create()
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'RemoveDebugStatements/dump' => true,
     ])
+    ->registerCustomFixers([new Drew\DebugStatementsFixers\Dump()])
+    ->setRiskyAllowed(true)
     ->setFinder($finder)
     ->setUsingCache(false)
 ;
