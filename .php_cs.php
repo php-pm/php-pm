b0aae62d995e03dd0cf523b8292404549bc793d5
andig <cpuidle@gmx.de>
2022-03-16 10:00:56 +0000

    Upgrade fixer (#550)

--- .php_cs.php
+++ .php_cs.php
@@ -1,16 +1,18 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
+require_once __DIR__ . '/vendor/autoload.php';
+
+$finder = (new PhpCsFixer\Finder())
     ->in('src')
 ;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
         'native_function_invocation' => ['strict' => true],
     ])
-    ->registerCustomFixers([new Drew\DebugStatementsFixers\Dump()])
+    // ->registerCustomFixers([new Drew\DebugStatementsFixers\Dump()])
     ->setRiskyAllowed(true)
     ->setFinder($finder)
     ->setUsingCache(false)
